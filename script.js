679ff99f9ac6b279730c8a73fce77931ca22a67a
saquib1915 <saquibalam884@gmail.com>
2026-09-21 23:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length <= 1) {
+        return -Infinity;
+    }
+
+    let highest = -Infinity;
+    let second = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > highest) {
+            second = highest;
+            highest = arr[i];
+        } else if (arr[i] > second && arr[i] < highest) {
+            second = arr[i];
+        }
+    }
+
+    return second;
+}
